cf419904a10b07d5b8fbd965b2055d4b5e273ff4
MrEnde <mrendor.q@gmail.com>
2021-06-07 19:36:21 +0300

    Renamed class App to Application

--- server/src/main/java/org/cloud/server/Runner.java
+++ server/src/main/java/org/cloud/server/Runner.java
@@ -1,8 +1,10 @@
 package org.cloud.server;
 
+import org.cloud.server.conf.Settings;
+
 public final class Runner {
     public static void main(String[] args) {
-        var app = new App();
-        app.run();
+        var app = new Application(Settings.getSettings());
+        app.start();
     }
 }
