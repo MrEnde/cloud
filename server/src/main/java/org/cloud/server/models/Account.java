087bc41c394ab28e72ce87582fc0db8ceb1b3308
MrEnde <mrendor.q@gmail.com>
2021-06-07 19:37:33 +0300

    Refactored Account.java

--- server/src/main/java/org/cloud/server/models/Account.java
+++ server/src/main/java/org/cloud/server/models/Account.java
@@ -1,8 +1,6 @@
 package org.cloud.server.models;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import org.hibernate.annotations.GenericGenerator;
@@ -11,56 +9,38 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Builder
+@AllArgsConstructor
+@Data
 @Entity
 @Table(name = "accounts")
 public class Account implements Model {
 
     public Account() { }
 
-    @Setter
-    @Getter
-    @NotNull
     @Column(name = "first_name", nullable = false)
     @Size(min = 1, max = 50)
     private String firstName;
 
-    @Setter
-    @Getter
-    @NotNull
     @Column(name = "last_name", nullable = false)
     @Size(min = 1, max = 50)
     private String lastName;
 
-    @Setter
-    @Getter
-    @NotNull
     @Column(name = "nick", nullable = false, unique = true)
     @Size(min = 4, max = 17)
     private String nick;
 
-    @Setter
-    @Getter
-    @NotNull
     @Column(name = "login", nullable = false, unique = true)
     @Size(min = 6, max = 30)
     private String login;
 
-    @Setter
-    @Getter
-    @NotNull
     @Column(name = "password", nullable = false)
     @Size(min = 8, max = 30)
     private String password;
 
-    @Setter
-    @Getter
-    @NotNull
     @Column(name = "data_joined", nullable = false)
     @Temporal(TemporalType.DATE)
     private Date dateJoined;
 
-    @Setter
-    @Getter
     @Id
     @GeneratedValue(generator="increment")
     @GenericGenerator(name="increment", strategy = "increment")
