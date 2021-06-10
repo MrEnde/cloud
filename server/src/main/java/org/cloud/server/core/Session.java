742e31d43f6558614465d4a4fe9aa8f7dabd28c0
MrEnde <mrendor.q@gmail.com>
2021-06-10 20:11:38 +0300

    Redesign of the session

--- server/src/main/java/org/cloud/server/core/Session.java
+++ server/src/main/java/org/cloud/server/core/Session.java
@@ -1,4 +1,32 @@
 package org.cloud.server.core;
 
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class Session {
+    private final UUID id;
+    private final Channel channel;
+    private final Long userId;
+
+    private boolean closed;
+
+    public Session(Channel channel, Long userId) {
+        this.channel = channel;
+        this.userId = userId;
+        this.id = UUID.randomUUID();
+    }
+
+    public ChannelFuture close() {
+        closed = true;
+        return channel.close();
+    }
+
+    public boolean isConnected() {
+        return !closed && channel.isOpen() && channel.isActive();
+    }
+
 }
